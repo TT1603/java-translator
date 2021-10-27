7a5dcb9431ddc0b0e742fa855e9c2a25aa12b802
Thu <thutra@email.arizona.edu>
2021-10-26 21:43:30 -0700

    add test file

---
+++ Program2.java
@@ -0,0 +1,12 @@
+
+public class Program2{
+
+public static void main(String[] args){
+int x = arg[x];
+// Check if number is less than or equal to 1
+// Check if number is 2
+// Check if n is a multiple of 2
+// If not, then just check the odds
+int i = 3;
+}
+}
